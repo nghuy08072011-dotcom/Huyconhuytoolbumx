096fd6148cfb249e7162e0c57c8c848f644cb19e
nghuy08072011-dotcom <nghuy.08072011@gmail.com>
2026-02-11 20:15:19 +0700

    Update huy4 (1).py

--- huy4 (1).py
+++ huy4 (1).py
@@ -4,7 +4,7 @@ from sys import platform
 from datetime import datetime, timedelta
 from time import sleep
 
-LINK_LAY_KEY = "liên hệ số 0326111431 để lấy key free " 
+LINK_LAY_KEY = "liên hệ số ZALO 0326111431 để lấy key free " 
 SECRET_CODE = "0807" 
 
 den = '\x1b[1;90m'
